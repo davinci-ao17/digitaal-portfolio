182eab411175ceabc4e5fbc4f327c068b8ee179d
Vince den Besten <99043881@mydavinci.nl>
2018-01-10 13:53:09 +0100

    dotdotdot

---
+++ phone/javascriptphone.js
@@ -0,0 +1,12 @@
+
+
+function opleidingmenu() {
+  document.getElementById("cv").backgroundColor = "red";
+  document.getElementById("cv").innerHTML = "Terug";
+  document.getElementById("oplmenu").innerHTML = "Huidige opleiding";
+  document.getElementById("wrkmenu").innerHTML = "Vorige opleiding";
+}
+
+function werkmenu() {
+
+}
