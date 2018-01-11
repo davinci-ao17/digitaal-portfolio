6a7719d372fe64c40c55b5b6294a4d1ad5a7b509
Vince den Besten <99043881@mydavinci.nl>
2018-01-11 10:44:22 +0100

    phonemenu bug fixed

--- phone/javascriptphone.js
+++ phone/javascriptphone.js
@@ -1,12 +1,18 @@
 
 
 function opleidingmenu() {
-  document.getElementById("cv").backgroundColor = "red";
   document.getElementById("cv").innerHTML = "Terug";
+  document.getElementById("cv").onclick = back;
   document.getElementById("oplmenu").innerHTML = "Huidige opleiding";
+  setTimeout(function(){document.getElementById("oplmenu").href = "phone/pages/opleidingphone.html";},10);
   document.getElementById("wrkmenu").innerHTML = "Vorige opleiding";
+  document.getElementById("wrkmenu").href = "phone/pages/vorigeopleidingphone.html"
 }
 
 function werkmenu() {
+
+}
+
+function back() {
 
 }
